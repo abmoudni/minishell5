3c82403c13214181116252bbd0e3ee0cf9810f88
Mtawil <twlmed212@gmail.com>
2025-11-28 18:57:41 +0100

    fix

--- src/execution/heredoc.c
+++ src/execution/heredoc.c
@@ -1,14 +1,27 @@
-#include "../../include/minishell.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   heredoc.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtawil <mtawil@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/16 15:34:17 by mtawil            #+#    #+#             */
+/*   Updated: 2025/11/28 15:34:39 by mtawil           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 
 #include "../../include/minishell.h"
 
 char	*read_heredoc(char *delimiter)
 {
-	char *input;
-	char *filename;
-	int fd;
+	char	*input;
+	char	*filename;
+	int		fd;
 
 	filename = ft_strdup("/tmp/.heredoc_temp");
+	if (!filename)
+		return (NULL);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fd == -1)
 	{
@@ -16,24 +29,23 @@ char	*read_heredoc(char *delimiter)
 		free(filename);
 		return (NULL);
 	}
-
 	while (1)
-	{	
+	{
 		input = readline("> ");
 		if (!input)
+		{
+			write(1, "\n", 1);
 			break ;
+		}
 		if (ft_strcmp(input, delimiter) == 0)
 		{
 			free(input);
 			break ;
 		}
-
 		write(fd, input, ft_strlen(input));
 		write(fd, "\n", 1);
-
 		free(input);
 	}
-
 	close(fd);
 	return (filename);
 }
